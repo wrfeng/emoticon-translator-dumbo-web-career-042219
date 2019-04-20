ced08315b09482c825b7aa9335965eea39d4bee5
wei feng <theweifeng@gmail.com>
2019-04-20 18:43:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,8 +6,10 @@ def load_library(string)
   
   library.each do |meaning, array|
     hash[get_meaning] = {array[1] => meaning}
-    hash[get_emoticon] = {array
+    hash[get_emoticon] = {array[0] => array[1]}
   end
+  
+  hash
 end
 
 def get_japanese_emoticon
